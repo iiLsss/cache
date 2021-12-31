5b8a190282be8eb93705e49be099b3436a440036
iiLsss <lish1993@163.com>
2021-12-31 14:19:23 +0800

    feat: 增加ertg

--- server/3.cache-etag.js
+++ server/3.cache-etag.js
@@ -2,6 +2,7 @@ const http = require('http')
 const path = require('path')
 const url = require('url')
 const fs = require('fs').promises
+const crypto = require('crypto')
 
 let server = http.createServer(async (req, res) => {
   let { pathname } = url.parse(req.url)
@@ -10,9 +11,23 @@ let server = http.createServer(async (req, res) => {
     let statObj = await fs.stat(absPath)
     if (statObj.isDirectory()) {
       absPath = path.join(absPath, 'index.html')
+      await fs.access(absPath)
     }
+    let content = await fs.readFile(absPath, 'utf-8')
+    if (req.url.match(/css/)) {
+      // 利用文件内容生成 
+      let hash = crypto.createHash('md5').update(content).digest('base64')
+      let ifNoneMatch = req.headers['if-none-match']
+      res.setHeader('ETag', hash)
+      if (ifNoneMatch === hash) {
+        res.statusCode = 304
+        return res.end()
+      }
+    }
+    res.end(content)
   } catch (error) {
-    
+    console.log(error)
+    res.end('Not found') 
   }
 })
 
